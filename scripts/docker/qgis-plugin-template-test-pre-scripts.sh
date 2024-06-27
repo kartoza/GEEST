0155efbff689c286152b593849cc77939eb8e7ce
Samweli <smwltwesa6@gmail.com>
2024-06-27 19:46:03 +0300

    include ci configurations

--- scripts/docker/qgis-plugin-template-test-pre-scripts.sh
+++ scripts/docker/qgis-plugin-template-test-pre-scripts.sh
@@ -2,7 +2,7 @@
 
 qgis_setup.sh
 
-# FIX default installation because the sources must be in "qgis_templates_symbology" parent folder
-rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis-templates-symbology
-ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/qgis-templates-symbology
-ln -sf /tests_directory /usr/share/qgis/python/plugins/qgis-templates-symbology
+# FIX default installation because the sources must be in "plugin-name" parent folder
+rm -rf  /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/plugin-name
+ln -sf /tests_directory /root/.local/share/QGIS/QGIS3/profiles/default/python/plugins/plugin-name
+ln -sf /tests_directory /usr/share/qgis/python/plugins/plugin-name
